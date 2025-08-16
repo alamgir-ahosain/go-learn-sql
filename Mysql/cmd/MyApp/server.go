63e248e4a7c6053d4ed1ed173ce4615c28c6f703
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-16 21:47:40 +0600

    direct connect with the main

---
+++ Mysql/cmd/MyApp/server.go
@@ -0,0 +1,27 @@
+package myapp
+
+import (
+	"fmt"
+
+	"github.com/alamgir-ahosain/learn-sql/Mysql/controllers"
+	"github.com/alamgir-ahosain/learn-sql/Mysql/db"
+)
+
+func RunApp() {
+	db.Connect()
+	defer db.DB.Close() //close the DB connection when main exist
+
+	//Create user
+	controllers.CreateUser("CE21012", "Alamgir", 3.48)
+
+	//Read user By Id
+	user, err := controllers.ReadUserById(3)
+	controllers.CheckError(err)
+	fmt.Println(user)
+
+	//Read Multiple user by Field
+	users,er:=controllers.ReadUserByField("CE21012")
+	controllers.CheckError(er)
+	fmt.Println(users)
+
+}
